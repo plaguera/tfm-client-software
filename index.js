31cbc3ef2a71be9c5720e9a91890c90ede18e8c5
Pedro Lagüera <laguerapedro@gmail.com>
2020-04-01 19:06:57 +0100

    Changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 const PORT = process.env.PORT || 3000;
 
 var app = express();
-app.use('/client.js', staticFile(path.join(process.cwd(), 'dist/client.js'), {}));
+//app.use('/client.js', staticFile(path.join(process.cwd(), 'dist/client.js'), {}));
+app.use('/', express.static(path.join(process.cwd(), 'dist')))
 app.get('/', (req, res) => res.send('Serving /client.js'));
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
