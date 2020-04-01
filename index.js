54a3b592a1577d637a90e19a95373ab876efb335
Pedro Lagüera <laguerapedro@gmail.com>
2020-04-01 19:04:15 +0100

    Fix static path

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var staticFile = require('connect-static-file');
+var path = require('path');
 var express = require('express');
 const PORT = process.env.PORT || 3000;
 
 var app = express();
-app.use('/client.js', staticFile(`dist/client.js`, {}));
+app.use('/client.js', staticFile(path.join(process.cwd(), 'dist/client.js'), {}));
 app.get('/', (req, res) => res.send('Serving /client.js'));
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
